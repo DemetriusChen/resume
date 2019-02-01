5d5fbdf335b02a067165d5c5afc3b7e3718f81d6
ease <3532753799@qq.com>
2019-02-01 13:42:49 +0800

    更新简历信息，用vue渲染技能数据

---
+++ js/skillData.js
@@ -0,0 +1,37 @@
+let vm = new Vue({
+    el: "#vue-skills",
+    data: {
+        skills: [
+            {
+                id: 1,
+                name: 'JavaScript',
+                percentage: 66
+            },
+            {
+                id: 2,
+                name: 'DOM',
+                percentage: 53
+            },
+            {
+                id: 3,
+                name: 'CSS',
+                percentage: 56
+            },
+            {
+                id: 4,
+                name: 'nodeJS',
+                percentage: 59
+            },
+            {
+                id: 5,
+                name: 'Express',
+                percentage: 51
+            },
+            {
+                id: 6,
+                name: 'MongoDB',
+                percentage: 70
+            },
+        ]
+    }
+})
